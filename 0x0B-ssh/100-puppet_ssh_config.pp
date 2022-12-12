39109ecd2131eb1349ba3cfc60ccaa6a52955423
Ikenna <ikenna@kali.Ikenna-PC>
2022-12-12 03:50:03 -0500

    (fixed 0.2) Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,20 +3,14 @@
 # - to use the private key ~/.ssh/school
 # Your SSH client configuration must be configured 
 # - to refuse to authenticate using a password
-include stdlib
 
-file_line { 'NO password':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => '    PasswordAuthentication no',
-  match              => '^PasswordAuthentication',
-  append_on_no_match => true,
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
-
-file_line{ 'New private key holder':
-  ensure             => present,
-  path               => 'etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^IdentityFile',
-  append_on_no_match => true
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
 }
