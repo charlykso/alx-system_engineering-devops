9f12642d42054fde92692f6c2c470ef091812263
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-17 00:16:36 -0500

    (fixed 1.0) Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,27 @@ returns information about his/her TODO list progress.
 """
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     import requests
     from sys import argv
 
-
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    for u in users.json():
+        if u.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (u.get('name'))
+            break
+    TOTAL_NUM_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
-    USER_ID = argv[1]
-    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                       format(USER_ID)).json()
-    USERNAME = req.get("username")
-    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                       format(USER_ID)).json()
-    for item in req:
-        if item.get("completed") is True:
-            TASK_TITLE.append(item.get('title'))
-            NUMBER_OF_DONE_TASKS += 1
-        TOTAL_NUMBER_OF_TASKS = len(req)
-
-    print('Employee {} is done with tasks({}/{}):'.
-          format(USERNAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TOTAL_NUM_OF_TASKS += 1
+            if t.get('completed') is True:
+                    NUMBER_OF_DONE_TASKS += 1
+                    TASK_TITLE.append(t.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUM_OF_TASKS))
     for title in TASK_TITLE:
         print('\t {}'.format(title))
